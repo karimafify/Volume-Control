305fa196b31e958a6ddbbad0c24ed75d27839b31
karimafify <karim.afifi.93@gmail.com>
2018-04-24 21:19:28 +0200

    Update volumeControl.c

--- volumeControl.c
+++ volumeControl.c
@@ -23,11 +23,15 @@
 ----------------------------------------------------------------*/
 void volume_control_update(void)
 {
+	static uint16_t prev_val=-1;
 	uint16_t adc_reading = adc_read_channel(SLIDER);
-	
+	if(adc_reading!=prev_val)
+	{
+		prev_val=adc_reading;
 	uint8_t duty_cycle_percentage = ( ((uint32_t)adc_reading * 100) / 1023 );
 	
 	timer2_pwm_duty_cycle_set(duty_cycle_percentage);
+	}
 }
 
 /*----------------------------------------------------------------
